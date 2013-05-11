985701819389c65fe34bee43aca0a9c58cdcfde2
jesuspereira <jesuspereira@192.168.0.16>
2013-05-11 13:20:28 +0200

    // clase que permite al usuario loggearse. A este estado se llega desde el boton "Log In" del menu

---
+++ src/game/LogInState2.java
@@ -0,0 +1,390 @@
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package game;
+
+import com.jme3.app.Application;
+import com.jme3.app.state.AbstractAppState;
+import com.jme3.app.state.AppStateManager;
+import com.jme3.asset.AssetManager;
+import com.jme3.audio.AudioRenderer;
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.input.FlyByCamera;
+import com.jme3.input.InputManager;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
+import com.jme3.material.RenderState;
+import com.jme3.niftygui.NiftyJmeDisplay;
+import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.ViewPort;
+import com.jme3.scene.Node;
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.builder.ImageBuilder;
+import de.lessvoid.nifty.builder.LayerBuilder;
+import de.lessvoid.nifty.builder.PanelBuilder;
+import de.lessvoid.nifty.builder.ScreenBuilder;
+import de.lessvoid.nifty.builder.TextBuilder;
+import de.lessvoid.nifty.controls.button.builder.ButtonBuilder;
+import de.lessvoid.nifty.controls.textfield.builder.TextFieldBuilder;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+import de.lessvoid.nifty.tools.Color;
+
+public class LogInState2 extends AbstractAppState implements ScreenController {
+
+    protected Node rootNode = new Node("Root Node");
+    protected Node guiNode = new Node("Gui Node");
+    protected BitmapText menuText;
+    protected BitmapFont menuFont;
+    private AssetManager assetManager;
+    private AppStateManager stateManager;
+    private InputManager inputManager;
+    private ViewPort viewPort;
+    private MainApp game = null;
+    private AudioRenderer audioRenderer;
+    private ViewPort guiViewPort;
+    private NiftyJmeDisplay niftyDisplay;
+    private FlyByCamera flyCam;
+    private Nifty nifty;
+    public static boolean b = false;
+
+    public LogInState2(MainApp game) {
+        this.game = game;
+    }
+
+    public void bind(Nifty nifty, Screen screen) {
+        this.nifty = nifty;
+    }
+
+    public void onStartScreen() {
+    }
+
+    public void onEndScreen() {
+    }
+
+    private class AppActionListener implements ActionListener {
+
+        public void onAction(String name, boolean value, float tpf) {
+            if (!value) {
+                return;
+            }
+
+        }
+    }
+
+    public void loadFPSText() {
+        menuFont = game.getAssetManager().loadFont("Interface/Fonts/Default.fnt");
+        menuText = new BitmapText(menuFont, false);
+        menuText.setSize(menuFont.getCharSet().getRenderedSize());
+        menuText.setLocalTranslation(0, (game.getContext().getSettings().getHeight() / 2f) - (menuText.getLineHeight() / 2f), 0);
+        menuText.setText("Frames per second");
+        guiNode.attachChild(menuText);
+    }
+
+    @Override
+    public void initialize(AppStateManager stateManager, Application app) {
+        super.initialize(stateManager, app);
+        this.game = (MainApp) game; // can cast Application to something more specific
+
+        this.assetManager = this.game.getAssetManager();
+        this.stateManager = this.game.getStateManager();
+        this.inputManager = this.game.getInputManager();
+        this.viewPort = this.game.getViewPort();
+        this.audioRenderer = this.game.getAudioRenderer();
+        this.guiViewPort = this.game.getGuiViewPort();
+        this.flyCam = new FlyByCamera(game.getCamera());
+
+        // enable depth test and back-face culling for performance
+        game.getRenderer().applyRenderState(RenderState.DEFAULT);
+
+
+        // Init input
+        if (game.getInputManager() != null) {
+            game.getInputManager().addMapping("SIMPLEAPP_Exit1", new KeyTrigger(KeyInput.KEY_0));
+        }
+
+
+
+        niftyDisplay = new NiftyJmeDisplay(
+                assetManager, inputManager, audioRenderer, guiViewPort);
+
+
+        nifty = niftyDisplay.getNifty();
+        guiViewPort.addProcessor(niftyDisplay);
+        flyCam.setDragToRotate(true);
+        nifty.loadStyleFile("nifty-default-styles.xml");
+        nifty.loadControlFile("nifty-default-controls.xml");
+
+        inputManager.setCursorVisible(true);
+
+
+
+        nifty.addScreen("LogInScreen2", new ScreenBuilder("LogInScreen2") {
+            {
+                //controller(new GUI.PowdersScreenController()); // This connects the Java class StartingScreen and the GUI screen.     
+                controller(new MenuState(game));
+
+
+                layer(new LayerBuilder("LayerLogIn") {
+                    {
+                        childLayoutVertical(); // layer properties, add more...
+
+                        // <panel_1>
+                        panel(new PanelBuilder("Panel_Title_Powders") {
+                            {
+                                childLayoutCenter();
+                                alignCenter();
+                                height("15%");
+                                width("100%");
+                                
+                            }
+                        }); // </panel_1>
+
+                        panel(new PanelBuilder("Panel_Title_LogIn") {
+                            {
+                                childLayoutCenter();
+                                alignCenter();
+                                height("10%");
+                                width("50%");
+
+                                // add text
+                                text(new TextBuilder() {
+                                    {
+                                        text("LOG IN");
+                                        font("Interface/Fonts/Default.fnt");
+                                        height("100%");
+                                        width("100%");
+                                    }
+                                });
+                            }
+                        }); // </panel_1>
+
+                        // <panel_2>
+                        panel(new PanelBuilder("Panel_RegisterProcess") {
+                            {
+                                childLayoutVertical(); // panel properties, add more...               
+                                alignCenter();
+                                valignCenter();
+                                height("50%");
+                                width("60%");
+
+                                panel(new PanelBuilder("Panel_Text") {
+                                    {
+                                        childLayoutVertical();
+                                        height("30%");
+                                        width("50%");
+                                        alignCenter();
+                                        valignCenter();
+
+                                        text(new TextBuilder() {
+                                            {
+                                                alignCenter();
+                                                valignCenter();
+                                                font("Interface/Fonts/Default.fnt");
+                                                text("Insert your nick and password if you are alredy registered.");
+                                                height("20%");
+                                                width("80%");
+                                            }
+                                        });
+
+                                        text(new TextBuilder() {
+                                            {
+                                                alignCenter();
+                                                valignCenter();
+                                                text("If not, go \"BACK\" press the \"REGISTER\" button in the main menu.");
+                                                font("Interface/Fonts/Default.fnt");
+                                                height("20%");
+                                                width("80%");
+                                            }
+                                        });
+                                    }
+                                }); // </panel_2>
+
+
+                                panel(new PanelBuilder("Panel_InsertData1") {
+                                    {
+                                        childLayoutHorizontal(); // panel properties, add more...               
+                                        height("7%");
+                                        width("50%");
+                                        alignCenter();
+                                        valignCenter();
+
+                                        text(new TextBuilder() {
+                                            {
+                                                alignCenter();
+                                                valignCenter();
+                                                text("NICK: ");
+                                                font("Interface/Fonts/Default.fnt");
+                                                height("70%");
+                                                width("30%");
+                                            }
+                                        });
+
+                                        control(new TextFieldBuilder("NickLogIn") {
+                                            {
+                                                width("50%");
+                                                height("100%");
+                                            }
+                                        });
+                                    }
+                                });
+
+                                panel(new PanelBuilder("Panel_Empty") {
+                                    {
+                                        childLayoutHorizontal(); // panel properties, add more...               
+                                        alignCenter();
+                                        valignCenter();
+                                        height("3%");
+                                        width("50%");
+
+                                    }
+                                });
+
+
+                                panel(new PanelBuilder("Panel_InsertData12") {
+                                    {
+                                        childLayoutHorizontal(); // panel properties, add more...               
+                                        alignCenter();
+                                        valignCenter();
+                                        height("7%");
+                                        width("50%");
+
+                                        text(new TextBuilder() {
+                                            {
+                                                alignCenter();
+                                                valignCenter();
+                                                text("PASS: ");
+                                                font("Interface/Fonts/Default.fnt");
+                                                height("70%");
+                                                width("30%");
+                                            }
+                                        });
+
+                                        control(new TextFieldBuilder("PassLogIn") {
+                                            {
+                                                width("50%");
+                                                height("100%");
+                                            }
+                                        });
+                                    }
+                                }); // </panel_2>
+
+                                panel(new PanelBuilder("Panel_Buttons") {
+                                    {
+                                        childLayoutHorizontal(); // panel properties, add more...               
+                                        alignCenter();
+                                        valignCenter();
+                                        height("30%");
+                                        width("50%");
+
+                                        control(new ButtonBuilder("Button_LOGIN", "LOG IN") {
+                                            {
+                                                alignCenter();
+                                                valignBottom();
+                                                backgroundColor("#f108");
+                                                height("35%");
+                                                width("45%");
+                                                visibleToMouse(true);
+                                                interactOnClick("cargarUsuario2()");
+
+                                            }
+                                        });
+
+                                        panel(new PanelBuilder("Panel_Space") {
+                                            {
+                                                childLayoutHorizontal(); // panel properties, add more...               
+                                                alignCenter();
+                                                valignCenter();
+                                                height("4%");
+                                                width("10%");
+
+                                            }
+                                        });
+
+                                        control(new ButtonBuilder("Button_QUIT", "BACK") {
+                                            {
+                                                alignCenter();
+                                                valignBottom();
+                                                backgroundColor("#f108");
+                                                height("35%");
+                                                width("45%");
+                                                visibleToMouse(true);
+                                                interactOnClick("loadMenuFromLogIn2()");
+
+                                            }
+                                        });
+
+                                    }
+                                });
+
+                            }
+                        }); // </panel_2>
+                    }
+                });
+                // </layer>
+            }
+        }.build(nifty));
+        // </screen>
+
+        game.getGUIViewPort().addProcessor(niftyDisplay);
+        nifty.gotoScreen("LogInScreen2"); // it is used to start the screen
+    }
+
+    public void update(float tpf) {
+        super.update(tpf);
+
+
+
+        // simple update and root node
+
+        rootNode.updateLogicalState(tpf);
+        guiNode.updateLogicalState(tpf);
+        rootNode.updateGeometricState();
+        guiNode.updateGeometricState();
+
+    }
+
+    @Override
+    public void stateAttached(AppStateManager stateManager) {
+        super.stateAttached(stateManager);
+        //  game.getInputManager().addListener(new AppActionListener(), "SIMPLEAPP_Exit1");
+        game.getViewPort().attachScene(rootNode);
+        game.getGUIViewPort().attachScene(guiNode);
+        if (b == true) {
+            game.getGUIViewPort().addProcessor(niftyDisplay);
+        }
+//      
+    }
+
+    @Override
+    public void stateDetached(AppStateManager stateManager) {
+        super.stateDetached(stateManager);
+        game.getViewPort().detachScene(rootNode);
+        game.getGUIViewPort().detachScene(guiNode);
+        game.getGUIViewPort().removeProcessor(niftyDisplay);
+    }
+
+    public void render(RenderManager rm) {
+    }
+
+    public void startInput() {
+
+        //nifty.exit();
+        nifty.removeScreen("MenuScreen");
+        game.loadInput();
+
+
+    }
+
+    public void quitInput() {
+        game.stop();
+    }
+
+    public void loadInput() {
+        game.loadInput();
+    }
+}
